e4ad7c955e45f0e2ea7e2ace9ad871b2102f86d6
Manumarfo <owurakumanumarfo@gmail.com>
2022-06-30 18:36:33 -0700

    cprog

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Return always 0 (Success)
+ * Return: 0 Always (Success)
  */
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
