f2bcd6b1d5568635baad97bb8e5b254f49d77874
Manumarfo <owurakumanumarfo@gmail.com>
2022-07-06 04:49:31 -0700

    nested

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
