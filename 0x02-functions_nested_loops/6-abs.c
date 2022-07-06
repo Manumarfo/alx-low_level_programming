2bb8c4059aae45cd18a32b5803ef29b185752688
Manumarfo <owurakumanumarfo@gmail.com>
2022-07-06 08:36:13 -0700

    nested

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of a number
+ * @int: data type
+ * Return: always 0.
+ */
+int _abs(int)
+{
+
+	if (int > 0)
+		return (int * 1);
+	else if (int < 0)
+		return (int * -1);
+	return (0);
+}
