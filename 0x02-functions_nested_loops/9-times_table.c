4c1d0107b83655ba525948def277e7dd0c3354a8
Manumarfo <owurakumanumarfo@gmail.com>
2022-07-06 17:23:19 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (col = 0; col <= 9; col++)
+		for (col = 1; col <= 9; col++)
 		{
 			res = row * col;
 			if ((res / 10) > 0)
