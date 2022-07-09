4ec76910bdf3644ace5ea41827805d85a6c5da2a
Manumarfo <owurakumanumarfo@gmail.com>
2022-07-08 18:50:24 -0700

    more

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,25 @@
  */
 void print_triangle(int size)
 {
-        if (size <= 0)
-        {
-                _putchar('\n');
-        } else
-        {
-                int space, row, symbol;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int space, row, symbol;
 
-                for (row = 1; row <= size; row++)
-                {
-                        for (space = 1; space <= (size - row); space++)
-                        {
-                                _putchar(' ');
-                        }
-
-                        for (symbol = 1; symbol <= row; symbol++)
-                        {
-                                _putchar('#');
-                        }
-
-                        _putchar('\n');
-                }
-        }
+		for (row = 1; row <= size; row++)
+		{
+			for (space = 1; space <= (size - row); space++)
+			{
+				_putchar(' ');
+			}
+			for (symbol = 1; symbol <= row; symbol++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
